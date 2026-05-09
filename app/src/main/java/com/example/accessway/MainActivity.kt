405c8a5e61eb9f96a47f5110ba18013e2012b4f7
LuizPav <luizpavao35@gmail.com>
2026-05-09 01:08:27 -0300

    [FEAT] SearchBar UI

--- app/src/main/java/com/example/accessway/MainActivity.kt
+++ app/src/main/java/com/example/accessway/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,8 +13,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.accessway.ui.Details
+import com.example.accessway.ui.SearchBar
 import com.example.accessway.ui.theme.AccessWayTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,31 +38,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
-    Column() {
-//     SearchBar
-        Row() {
-
-        }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        SearchBar()
 //     Mapa
         Box() {
 
         }
 //     Detalhes
-        Column() {
-            Row() {
-//            Logo Avaliação
-                Box() {
-
-                }
-//            Detalhes
-                Column() {
-
-                }
-//            Avaliação
-                Box() {
-
-                }
-            }
-        }
+        Details()
     }
 }
---
+++ app/src/main/java/com/example/accessway/ui/Details.kt
@@ -0,0 +1,26 @@
+package com.example.accessway.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Details() {
+    Column() {
+        Row() {
+//            Logo Avaliação
+            Box() {
+
+            }
+//            Detalhes
+            Column() {
+
+            }
+//            Avaliação
+            Box() {
+
+            }
+        }
+    }
+}
